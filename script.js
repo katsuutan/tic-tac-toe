e05a0d0f8484feaa38cec6b38667fe7cef07d553
Jason <katsuutan0408@gmail.com>
2024-05-04 17:42:13 -0700

    Finish tic tac toe in console only version

--- script.js
+++ script.js
@@ -89,10 +89,38 @@ const gameboard = (function () {
             case 9:
                 board[2][2] = symbol;
         }
-        console.log ('placeSymbol ran correctly');
     };
 
-    return {getBoard, displayBoard, placeSymbol, checkValidLocation};
+    // Should run after placeSymbol is ran to check the current board for a winner.
+    const checkWinner = () => {
+        // Check the 3 columns for winner
+        for (let i = 0; i < 3; i++) {
+            if (board[0][i] != null) {
+                if (board[0][i] === board[1][i] && board[0][i] === board[2][i]) {
+                    return board[0][i];
+                }
+            }
+        }
+
+        // Check the 3 rows for winner
+        for (let j = 0; j < 3; j++) {
+            if (board[j][0] != null) {
+                if (board[j][0] === board[j][1] && board[j][0] === board[j][2]) {
+                    return board[j][0];
+                }
+            }
+        }
+        
+        // Checks the diagonal for winner
+        if (board[0][0] === board[1][1] && board[0][0] === board[2][2])
+            return board[0][0]
+        if (board[0][2] === board[1][1] && board[0][2] === board[2][0])
+            return board[0][2];
+
+        return null; // Returns null (a falsy value) if no winners yet.
+    };
+
+    return {getBoard, displayBoard, placeSymbol, checkValidLocation, checkWinner};
 })();
 
 
@@ -111,13 +139,15 @@ function createPlayer (token, name) {
 // GameController to control the flow and state of the game's turns, and checks if there is a winner.
 function gameController(playerOneName = 'Player One', playerTwoName = 'Player Two') {
     let board = gameboard;
+    let turnCount = 0;
 
     const playerOne = createPlayer('O', playerOneName);
     const playerTwo = createPlayer('X', playerTwoName);
     
 
-    let activePlayer = playerOne;
+    let activePlayer = playerOne; // Default sets to player one as one will always go first.
 
+    // Used to swap the the active player to alternate turns.
     const alternateTurns = () => {
         if (activePlayer === playerOne)
             activePlayer = playerTwo;
@@ -126,27 +156,40 @@ function gameController(playerOneName = 'Player One', playerTwoName = 'Player Tw
     };
     const getActivePlayer = () => activePlayer;
 
+    // Displays current board and current player's turn.
     const updateBoard = () => {
         board.displayBoard();
         console.log(`${activePlayer.getPlayerName()}'s turn.`);
     };
 
+    // Runs every round
     const play = () => {
-        let location = prompt("Select a position for your token (value '1-9').");
-    
+        let location = prompt(`${activePlayer.getPlayerName}: Select a position for your token (value '1-9').`);
+
         let valid = board.checkValidLocation(location);
+        // If valid = true (empty cell)
         if (valid) {
             board.placeSymbol(parseInt(location), activePlayer.getSymbol());
 
+            // Checks for winner after token placement
+            if (board.checkWinner()) {
+                console.log(`${activePlayer.getPlayerName()} wins!`);
+                return 0; // Ends the play function call
+            }
+
             alternateTurns();
             updateBoard();
+            turnCount++;
         } else {
             console.log('There is already a token at that location. Please choose another one.');
-            updateBoard();
         }
+
+        // Checks for tie scenario
+        if (turnCount >= 9)
+            console.log("It's a tie!");
     };
 
-    updateBoard();
+    updateBoard(); // Initial board display
 
     return {play, getActivePlayer};
 };
